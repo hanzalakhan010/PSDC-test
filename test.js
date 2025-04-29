e76c959336cc81da4bb66ce5a6c56cae9b630088
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-04-29 05:45:08 +0000

    Done

---
+++ test.js
@@ -0,0 +1,187 @@
+// // A)
+
+// // function testScope() {
+
+// //     var x = 10;
+
+// //     if (true) {
+
+// //         var x = 20;
+
+// //     }
+
+// //     console.log(x);
+
+// // }
+
+// // testScope();
+
+// // B)
+
+// // function testScope() {
+
+// //     let x = 10;
+
+// //     if (true) {
+
+// //         let x = 20;
+
+// //     }
+
+// //     console.log(x);
+
+// // }
+
+// // testScope();
+
+
+
+
+
+// // C)
+// // function testScope() {
+
+// //     var x = 10;
+
+// //     if (true) {
+
+// //         let x = 20;
+
+// //     }
+
+// //     console.log(x);
+
+// // }
+
+// // testScope();
+
+// // D)
+
+// // function testScope() {
+
+// //     const x = 10;
+
+// //     if (true) {
+
+// //         const x = 20;
+
+// //     }
+
+// //     console.log(x);
+
+// // }
+
+// // testScope();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function outerFunction() {
+
+//     var outerVar = 'Hello';
+  
+    
+  
+//     function innerFunction() {
+  
+//       console.log(outerVar);  
+  
+//     }
+  
+    
+  
+//     innerFunction();
+  
+//   }
+  
+//   outerFunction();
+  
+  
+//   B)
+//   function outerFunction() {
+  
+//     var outerVar = 'Hello';
+  
+    
+  
+//     function innerFunction() {
+  
+//       let outerVar = 'World';
+//   function
+  
+//       console.log(outerVar);  
+  
+//     }
+  
+    
+  
+//     innerFunction();
+  
+//   }
+  
+//   outerFunction();
+  
+//   C)
+//   function outerFunction() {
+  
+//     let outerVar = 'Hello';
+  
+    
+  
+//     function innerFunction() {
+  
+//       console.log(outerVar);  
+  
+//     }
+  
+    
+  
+//     innerFunction();
+  
+//   }
+  
+//   outerFunction();
+  
+//   D)
+//   function outerFunction() {
+  
+//     var outerVar = 'Hello';
+  
+    
+  
+//     function innerFunction() {
+  
+//       var outerVar = 'World'; 
+//   name
+  
+//       console.log(outerVar);  
+  
+//     }
+  
+    
+  
+//     innerFunction();
+  
+//   }
+  
+//   outerFunction();
+// let x
+// console.log(typeof x)
+// Array().map
+// console.log(d)
+function na(){
+    console.log(x)
+    var x = ''
+}
+na()
---
+++ todo_app/script.ts
@@ -0,0 +1,72 @@
+interface TODOList {
+    id: number,
+    title: string
+}
+
+class TODO {
+    todos: TODOList[]
+    constructor() {
+        this.todos = []
+    }
+    addTodo() {
+        let todo = (document.getElementById('todoInput') as HTMLInputElement)
+        if (todo.value) {
+            this.todos.push({
+                id: this.todos.length,
+                title: todo.value
+            })
+            todo.value = ''
+            this.renderState()
+        }
+        else {
+            alert('Empty value')
+        }
+    }
+    editTodo(id: number) {
+        let todo = this.todos.find(todo => todo.id == id)
+        let selectedDiv = document.getElementById(`${id}-DIV`)
+        if (selectedDiv && todo) {
+            selectedDiv.innerHTML = `
+            <div id = '${todo.id}-DIV'>
+                <p id = '${todo.id}-p' contenteditable>${todo.title}</p>
+                <button onclick = 'todos.saveTodo(${todo.id})'>Save</button>
+            </div>
+            `
+        }
+    }
+    saveTodo(id: number) {
+        let pTag = document.getElementById(`${id}-p`)
+        if (pTag) {
+            for (let todo of this.todos) {
+                if (todo.id == id) {
+                    todo.title = pTag.innerText
+                    this.renderState()
+                    break
+                }
+            }
+        }
+
+    }
+    deleteTodo(id: number) {
+        console.log(id)
+        this.todos = this.todos.filter(todo => todo.id != id)
+        this.renderState()
+    }
+    renderState() {
+        let todoDiv = document.getElementById('todos')
+        if (todoDiv) {
+            todoDiv.innerHTML = ''
+            this.todos.forEach((todo) => {
+                todoDiv?.insertAdjacentHTML('beforeend', `
+                    <div id = '${todo.id}-DIV'>
+                        <p id = '${todo.id}-p'>${todo.title}</p>
+                        <button onclick = 'todos.editTodo(${todo.id})'>Edit</button>
+                        <button onclick = 'todos.deleteTodo(${todo.id})'>Delete</button>
+                    </div>
+                    `)
+            })
+        }
+    }
+}
+
+let todos = new TODO()
